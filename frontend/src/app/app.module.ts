f29ff7cc0413f82887fbf2ff360295d0a09090a8
Skrigan <sk_dm-821@mail.ru>
2023-03-06 19:32:36 +0300

    add authorization/registration form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    AuthorizationComponent
   ],
   imports: [
     BrowserModule,
